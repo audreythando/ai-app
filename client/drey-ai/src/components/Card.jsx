6210eb4aeac1f7ef666531c33b1b52023bd284d9
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-02-19 19:41:20 +0200

    refactor: installed packages to server

--- client/drey-ai/src/components/Card.jsx
+++ client/drey-ai/src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-import { logo } from './assets';
-import { Home, CreatePost } from './page';
+import { download } from '../assets';
+import { downloadImage } from '../utils';
 
-const App = () => (
-  <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
-      </Link>
+const Card = ({ _id, name, prompt, photo }) => (
+  <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+    <img
+      className="w-full h-auto object-cover rounded-xl"
+      src={photo}
+      alt={prompt}
+    />
+    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+      <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
-    </header>
-    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
-      </Routes>
-    </main>
-  </BrowserRouter>
+      <div className="mt-5 flex justify-between items-center gap-2">
+        <div className="flex items-center gap-2">
+          <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">{name[0]}</div>
+          <p className="text-white text-sm">{name}</p>
+        </div>
+        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
+          <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
+        </button>
+      </div>
+    </div>
+  </div>
 );
 
-export default App;
+export default Card;
