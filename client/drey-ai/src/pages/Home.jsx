098aef8a74237721c6bd8b269d1533120b04c683
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-02-19 23:55:18 +0200

    refactor: changed url

--- client/drey-ai/src/pages/Home.jsx
+++ client/drey-ai/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
       setLoading(true);
   
       try {
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        const response = await fetch('http://localhost:8060/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
