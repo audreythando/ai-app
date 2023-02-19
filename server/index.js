13c7a62b7bfb7e1b8942df6d7f2b3023554bbf64
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-02-19 23:37:17 +0200

    refactor: intergrating frontend

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dreyRoutes from './routes/dreyRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/drey', dreyRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from DREY.AI')
@@ -14,8 +21,15 @@ app.get('/', async (req, res) => {
   
 
   const startServer = async () => {
-      app.listen(8080, () => console.log('Server has started on port http://localhost:8080')
-      )
+
+    try {
+        connectDB(process.env.MongoDB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    }catch (error){
+console.log(error);
+    }
+
+     
   }
       
   startServer();
